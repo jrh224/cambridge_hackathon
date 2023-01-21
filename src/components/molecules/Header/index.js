2c2b706c1245e180933b513d14a2e5ef8bbf83c0
Daniel S <mcds606@gmail.com>
2023-01-21 10:00:36 +0000

    Header

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header">
-      <h1> This is a header auifhweiufiwebfsdnf</h1>
+      <h1> This is a header</h1>
     </div>
   );
 }
